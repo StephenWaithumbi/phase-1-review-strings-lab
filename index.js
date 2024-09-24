5ec92b222f5afbaf1c28667a0c8e728df93db483
Stephen Waithumbi <stephenwaithumbi14@gmail.com>
2024-09-24 08:47:05 +0300

    Done with the labs

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = "Steven"
+const welcomeMessage = "Welcome to Flatbook, Steven!"
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, STEVEN!"
+const shortGreeting = 'Welcome, Steve!'
